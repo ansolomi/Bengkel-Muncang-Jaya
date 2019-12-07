ba2766357c524d4f7f82f9dcdc2bba34d86c4dfc
ikhsan0000 <46219884+ikhsan0000@users.noreply.github.com>
2019-12-08 02:28:43 +0700

    Update insert_own.php
    notify insert success added, fix some button, fix form length

--- insert_own.php
+++ insert_own.php
@@ -1,11 +1,12 @@
 <?php
 require_once "config.php";
-
+$posted = false;
 $id = $nama = $merk = $stock = $harga = '';
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     try 
     {
+		$posted = true;
         $jenis = $_POST['iJenis'];
         $merk = $_POST["iMerkBarang"];
         $tipe = $_POST["iTipe"];
@@ -14,9 +15,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $insert_into = "INSERT INTO spareparts(jenis,merk,tipe,harga) VALUES ('$jenis','$merk','$tipe','$harga')";
 
         pg_query($link, $insert_into);
-        echo '<script type="text/javascript">';
-        echo ' alert("Data"'.$jenis.'"Berhasil Dimasukkan")';  //not showing an alert box.
-        echo '</script>';
+
     }
     catch(Exception $e)
     {
@@ -28,47 +27,82 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<style>
+.nav{
+  position: relative;
+}
+  #left-panel-link {
+  position: absolute;
+  margin-left: 9.5%;
+}
+</style>
 	
     <meta charset="UTF-8">
     <title>Create Record</title>  
     <link rel="stylesheet" href="theme3.css" />  
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
+ <h1><center>Insert Spareparts<center></h1>
 </head>
 
 <body>
+	<?php
+	if($posted == true)
+	{
+        echo '<script type="text/javascript">';
+        echo 'alert("Data '.$jenis.' Berhasil Dimasukkan")';
+        echo '</script>';
+	}
+	
+	?>
+
 <form action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" METHOD="POST">  
-  
-     <div class="container">
-        
-            <br><br><br>
-			<div class="form-group">
-            <label for="iJenis">Jenis Spareparts:</label>
-            <input name = "iJenis" type="text" class="form-control">
-            </div>
-            
+ 
+   
 
-			<div class="form-group">
-            <label for="iMerkBarang">Merk Barang:</label>
-            <input name = "iMerkBarang" type="text" class="form-control">
-            </div>
-            
+	   <br>
+	
+		
+			<form class="text-center border border-light p-5" action="#!">
 			
-			<div class="form-group">
-            <label for="iTipe">Tipe Spareparts:</label>
-            <input name = "iTipe" type="text" class="form-control">
- 			</div>
-            
+			<center>
+			<div class="md-form">
+            <label for="iJenis">Jenis Spareparts:</label>
+            <input name = "iJenis" type="text" class="form-control mb-3 col-5">
+            </div>
+			</center>
 
+			<center>
+			<div class="md-form">
+            <label for="iMerkBarang">Merk Barang:</label>
+            <input name = "iMerkBarang" type="text" class="form-control mb-3 col-5">
+            </div>
+            </center>
+			
+			<center>
+			<div class="md-form">
+            <label for="iTipe">Tipe Spareparts:</label>
+            <input name = "iTipe" type="text" class="form-control mb-3 col-5">
+ 			</div>
+            </center>
+
+			<center>
 			<div class="form-group">
             <label for="iHarga">Harga Satuan:</label>
-            <input name = "iHarga" type="text" class="form-control">
+            <input name = "iHarga" type="text" class="form-control mb-3 col-5">
 			</div>
-    </div>
+			</center>
+			
+
 	<br><br>
 <center><input type="submit" class="btn btn-primary btn-lg" value="Submit"></center>
+</form>
 <br>
-<center><a type="submit" href="index.php" class="btn btn-lg" value="Finish">Finish</a></center>
+<center><a type="submit" href="index.php" class="btn btn-outline-primary btn-lg" value="Finish">Finish</a></center>
 </body>
 </html>
