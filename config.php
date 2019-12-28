3a43d4d61c3a919eb06b001965f5ee6a7e73f282
Alvin Genta Pratama <48116340+ansolomi@users.noreply.github.com>
2019-12-28 23:26:55 +0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* Attempt to connect to database */
-$link = pg_connect("host=localhost dbname=sbdtest user=postgres password=asdf");
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . "Error");
-}
-?>
-
